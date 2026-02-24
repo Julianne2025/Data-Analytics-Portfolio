1dd5525d2db3e3cc81f959b6ebf0a778d33ae1c0
Julianne2025 <julielorenzen@bellsouth.net>
2026-02-24 00:51:47 -0500

    Add files via upload

---
+++ SQL-Server/FreightAnalytics.sql
@@ -0,0 +1,49 @@
+USE FreightAnalytics
+GO
+
+SELECT*
+FROM dbo.FreightCOA
+
+SELECT*
+FROM dbo.FreightInvoice
+
+
+-- VALID GL CODE CHECK with LEFT JOIN
+SELECT
+    f.GLCode,
+    c.Description
+  FROM dbo.FreightInvoice f
+LEFT JOIN dbo.FreightCOA c
+    ON f.GLCode = c.GLCode;
+
+
+-- Total Freight by GL
+SELECT
+    f.GLCode,
+    c.Description,
+    SUM(f.[Paid Amount]) AS TotalFreight
+FROM dbo.FreightInvoice f
+LEFT JOIN dbo.FreightCOA c
+    ON f.GLCode = c.GLCode
+GROUP BY
+    f.GLCode,
+    c.Description
+ORDER BY TotalFreight DESC;
+
+
+-- Total Freight by Carrier
+SELECT
+    [Carrier Name],
+    SUM([Paid Amount]) AS TotalFreight
+FROM dbo.FreightInvoice
+GROUP BY [Carrier Name]
+ORDER BY TotalFreight DESC;
+
+
+-- Monthly Freight Trend
+SELECT
+    FORMAT([Ship Date], 'yyyy-MM') AS InvoiceMonth,
+    SUM([Paid Amount]) AS MonthlyFreight
+FROM dbo.FreightInvoice
+GROUP BY FORMAT([Ship Date], 'yyyy-MM')
+ORDER BY InvoiceMonth;
